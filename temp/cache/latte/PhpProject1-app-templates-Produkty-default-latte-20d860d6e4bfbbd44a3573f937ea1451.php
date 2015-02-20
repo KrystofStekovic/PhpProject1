84503642446bad231b3885a661e25b2c0ee5d228
KrystofStekovic <krystofstekovic@gmail.com>
2015-02-20 12:39:44 +0100

    neco

--- temp/cache/latte/PhpProject1-app-templates-Produkty-default-latte-20d860d6e4bfbbd44a3573f937ea1451.php
+++ temp/cache/latte/PhpProject1-app-templates-Produkty-default-latte-20d860d6e4bfbbd44a3573f937ea1451.php
@@ -2,50 +2,45 @@
 // source: C:\xampp\htdocs\PhpProject1\app/templates/Produkty/default.latte
 
 // prolog Latte\Macros\CoreMacros
-list($_b, $_g, $_l) = $template->initialize('2600289025', 'html')
+list($_b, $_g, $_l) = $template->initialize('3360449591', 'html')
 ;
 // prolog Latte\Macros\BlockMacros
 //
 // block content
 //
-if (!function_exists($_b->blocks['content'][] = '_lbecdb2be33a_content')) { function _lbecdb2be33a_content($_b, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
+if (!function_exists($_b->blocks['content'][] = '_lb7e80ee98d8_content')) { function _lb7e80ee98d8_content($_b, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
 ;if ($user->isInRole('admin')) { ?>
     <a href="<?php echo Latte\Runtime\Filters::escapeHtml($_control->link("Produkty:novy"), ENT_COMPAT) ?>
 ">Novy produkt</a>
-<?php } ?>
+<?php } $iterations = 0; foreach ($produkty as $produkt) { ?><div>
+    <?php echo Latte\Runtime\Filters::escapeHtml($produkt->nazev, ENT_NOQUOTES) ?>
 
-<table>
-<?php $iterations = 0; foreach ($produkty as $produkt) { ?>    <div>
-        <?php echo Latte\Runtime\Filters::escapeHtml($produkt->nazev, ENT_NOQUOTES) ?>
+    <?php echo Latte\Runtime\Filters::escapeHtml($produkt->popis, ENT_NOQUOTES) ?>
 
-        <?php echo Latte\Runtime\Filters::escapeHtml($produkt->popis, ENT_NOQUOTES) ?>
+    <?php echo Latte\Runtime\Filters::escapeHtml($produkt->cena, ENT_NOQUOTES) ?>
 
-        <?php echo Latte\Runtime\Filters::escapeHtml($produkt->cena, ENT_NOQUOTES) ?>
+    <?php echo Latte\Runtime\Filters::escapeHtml($produkt->id_materialu, ENT_NOQUOTES) ?>
 
-        <?php echo Latte\Runtime\Filters::escapeHtml($produkt->id_materialu, ENT_NOQUOTES) ?>
+    <?php echo Latte\Runtime\Filters::escapeHtml($produkt->mnozstvi, ENT_NOQUOTES) ?>
 
-        <?php echo Latte\Runtime\Filters::escapeHtml($produkt->mnozstvi, ENT_NOQUOTES) ?>
 
-        
-        <?php Nette\Bridges\FormsLatte\FormMacros::renderFormBegin($form = $_form = $_control["mnozstviForm"], array()) ?>
+    <?php Nette\Bridges\FormsLatte\FormMacros::renderFormBegin($form = $_form = $_control["mnozstviForm"], array()) ?>
 
-            <?php $_input = is_object($form['mnozstvi']) ? $form['mnozstvi'] : $_form[$form['mnozstvi']]; if ($_label = $_input->getLabel()) echo $_label->startTag() ;$_input = is_object($form['mnozstvi']) ? $form['mnozstvi'] : $_form[$form['mnozstvi']]; echo $_input->getControl() ;if ($_label) echo $_label->endTag() ?>
+    <?php $_input = is_object($form['mnozstvi']) ? $form['mnozstvi'] : $_form[$form['mnozstvi']]; if ($_label = $_input->getLabel()) echo $_label->startTag() ;$_input = is_object($form['mnozstvi']) ? $form['mnozstvi'] : $_form[$form['mnozstvi']]; echo $_input->getControl() ;if ($_label) echo $_label->endTag() ?>
 
-            <?php $_input = is_object($form['id_produktu']) ? $form['id_produktu'] : $_form[$form['id_produktu']]; echo $_input->getControl()->addAttributes(array('value'=>$produkt->id_produktu)) ?>
+    <?php $_input = is_object($form['id_produktu']) ? $form['id_produktu'] : $_form[$form['id_produktu']]; echo $_input->getControl()->addAttributes(array('value'=>$produkt->id_produktu)) ?>
 
-            <?php $_input = is_object($form['send']) ? $form['send'] : $_form[$form['send']]; echo $_input->getControl() ?>
+    <?php $_input = is_object($form['send']) ? $form['send'] : $_form[$form['send']]; echo $_input->getControl() ?>
 
-        <?php Nette\Bridges\FormsLatte\FormMacros::renderFormEnd($_form) ?>
+<?php Nette\Bridges\FormsLatte\FormMacros::renderFormEnd($_form) ?>
 
 <?php if ($user->isInRole('admin')) { ?>
-            <a href="<?php echo Latte\Runtime\Filters::escapeHtml($_control->link("Produkty:edit", array($produkt->id_produktu)), ENT_COMPAT) ?>
+    <a href="<?php echo Latte\Runtime\Filters::escapeHtml($_control->link("Produkty:edit", array($produkt->id_produktu)), ENT_COMPAT) ?>
 ">Editovat</a>
-            <a href="<?php echo Latte\Runtime\Filters::escapeHtml($_control->link("Produkty:delete", array($produkt->id_produktu)), ENT_COMPAT) ?>
+    <a href="<?php echo Latte\Runtime\Filters::escapeHtml($_control->link("Produkty:delete", array($produkt->id_produktu)), ENT_COMPAT) ?>
 ">Smazat</a>
 <?php } ?>
-    </div>
-<?php $iterations++; } ?>
-</table><?php
+</div><?php $iterations++; } 
 }}
 
 //
