4573d63d343f342dd7942d06746aba43a027fd14
KrystofStekovic <krystofstekovic@gmail.com>
2015-03-06 12:42:48 +0100

    sql

--- vlada.sql
+++ vlada.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 26, 2015 at 05:19 PM
+-- Generation Time: Mar 06, 2015 at 12:42 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -19,18 +19,13 @@ SET time_zone = "+00:00";
 --
 -- Database: `vlada`
 --
-CREATE DATABASE IF NOT EXISTS `vlada` DEFAULT CHARACTER SET utf8 COLLATE utf8_czech_ci;
-USE `vlada`;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `aktuality`
 --
--- Creation: Feb 22, 2015 at 09:41 PM
---
 
-DROP TABLE IF EXISTS `aktuality`;
 CREATE TABLE IF NOT EXISTS `aktuality` (
 `id_aktuality` int(11) NOT NULL,
   `nadpis` varchar(100) COLLATE utf8_czech_ci NOT NULL,
@@ -38,11 +33,6 @@ CREATE TABLE IF NOT EXISTS `aktuality` (
   `datum` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
---
--- Truncate table before insert `aktuality`
---
-
-TRUNCATE TABLE `aktuality`;
 --
 -- Dumping data for table `aktuality`
 --
@@ -56,22 +46,14 @@ INSERT INTO `aktuality` (`id_aktuality`, `nadpis`, `text`, `datum`) VALUES
 --
 -- Table structure for table `clanky`
 --
--- Creation: Feb 22, 2015 at 09:41 PM
---
 
-DROP TABLE IF EXISTS `clanky`;
 CREATE TABLE IF NOT EXISTS `clanky` (
 `id_clanku` int(11) NOT NULL,
   `datum` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `nadpis` varchar(100) COLLATE utf8_czech_ci NOT NULL,
   `text` text COLLATE utf8_czech_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
---
--- Truncate table before insert `clanky`
---
-
-TRUNCATE TABLE `clanky`;
 --
 -- Dumping data for table `clanky`
 --
@@ -79,82 +61,54 @@ TRUNCATE TABLE `clanky`;
 INSERT INTO `clanky` (`id_clanku`, `datum`, `nadpis`, `text`) VALUES
 (1, '2015-02-10 17:20:33', 'Prvni clanek', 'Text prvniho clanku'),
 (2, '2015-02-13 12:26:19', 'Mujj clanek', 'nfsadn  kgmdsa kfmdskl mfkdsm kfma kfmd;l'),
-(3, '2015-02-20 12:39:09', 'glkfgmksldgkls d', 'gfsjzbermglkrsmgbldsf¨¨)h\n \nh\n\nhrthbrtzhdt\nghf\nhf\n\ngjgh');
+(3, '2015-02-20 12:39:09', 'glkfgmksldgkls d', 'hahaha\n \nh\n\nhrthbrtzhdt\nghf\nhf\n\ngjgh'),
+(4, '2015-03-03 10:17:32', 'ggg', '<p>toto je novzy clane&nbsp;</p>\n\n<p><strong>fsdfasdf</strong></p>\n\n<table border="1" cellpadding="1" cellspacing="1" style="width:500px">\n	<tbody>\n		<tr>\n			<td>gfdsg</td>\n			<td>fgsdfg</td>\n		</tr>\n		<tr>\n			<td>fdgsdfg</td>\n			<td>gsd</td>\n		</tr>\n		<tr>\n			<td>rewrefdsf</td>\n			<td>gsdfg</td>\n		</tr>\n	</tbody>\n</table>\n\n<p>&nbsp;</p>\n');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `kosiky`
 --
--- Creation: Feb 25, 2015 at 09:17 AM
---
 
-DROP TABLE IF EXISTS `kosiky`;
 CREATE TABLE IF NOT EXISTS `kosiky` (
 `id_kosiku` int(11) NOT NULL,
   `id_uzivatele` int(11) DEFAULT NULL,
   `datum_vytvoreni` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `otevreny` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
---
--- RELATIONS FOR TABLE `kosiky`:
---   `id_uzivatele`
---       `uzivatele` -> `id_uzivatele`
---
-
---
--- Truncate table before insert `kosiky`
---
-
-TRUNCATE TABLE `kosiky`;
 --
 -- Dumping data for table `kosiky`
 --
 
 INSERT INTO `kosiky` (`id_kosiku`, `id_uzivatele`, `datum_vytvoreni`, `otevreny`) VALUES
-(1, 4, '2015-02-25 21:59:18', 1);
+(1, 4, '2015-02-25 21:59:18', 1),
+(2, 3, '2015-03-05 11:42:06', 1);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `materialy`
 --
--- Creation: Feb 22, 2015 at 09:41 PM
---
 
-DROP TABLE IF EXISTS `materialy`;
 CREATE TABLE IF NOT EXISTS `materialy` (
 `id_materialu` int(11) NOT NULL,
   `nazev` varchar(200) COLLATE utf8_czech_ci NOT NULL,
   `mnozstvi` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
---
--- Truncate table before insert `materialy`
---
-
-TRUNCATE TABLE `materialy`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `obrazky`
 --
--- Creation: Feb 22, 2015 at 09:41 PM
---
 
-DROP TABLE IF EXISTS `obrazky`;
 CREATE TABLE IF NOT EXISTS `obrazky` (
 `id_obrazku` int(11) NOT NULL,
   `nazev` varchar(200) COLLATE utf8_czech_ci NOT NULL,
   `adresa` varchar(500) COLLATE utf8_czech_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
---
--- Truncate table before insert `obrazky`
---
-
-TRUNCATE TABLE `obrazky`;
 --
 -- Dumping data for table `obrazky`
 --
@@ -162,17 +116,15 @@ TRUNCATE TABLE `obrazky`;
 INSERT INTO `obrazky` (`id_obrazku`, `nazev`, `adresa`) VALUES
 (1, 'vybuch.png', 'images/vybuch.png'),
 (2, 'kredity.jpg', 'images/kredity.jpg'),
-(3, 'letadlo1.png', 'images/letadlo1.png');
+(3, 'letadlo1.png', 'images/letadlo1.png'),
+(4, 'ufo.png', 'images/ufo.png');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `produkty`
 --
--- Creation: Feb 25, 2015 at 09:23 AM
---
 
-DROP TABLE IF EXISTS `produkty`;
 CREATE TABLE IF NOT EXISTS `produkty` (
 `id_produktu` int(11) NOT NULL,
   `id_materialu` int(11) DEFAULT NULL,
@@ -184,19 +136,6 @@ CREATE TABLE IF NOT EXISTS `produkty` (
   `odecetMnozstvi` float NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
---
--- RELATIONS FOR TABLE `produkty`:
---   `id_obrazku`
---       `obrazky` -> `id_obrazku`
---   `id_materialu`
---       `materialy` -> `id_materialu`
---
-
---
--- Truncate table before insert `produkty`
---
-
-TRUNCATE TABLE `produkty`;
 --
 -- Dumping data for table `produkty`
 --
@@ -211,10 +150,7 @@ INSERT INTO `produkty` (`id_produktu`, `id_materialu`, `id_obrazku`, `nazev`, `p
 --
 -- Table structure for table `uzivatele`
 --
--- Creation: Feb 26, 2015 at 03:06 PM
---
 
-DROP TABLE IF EXISTS `uzivatele`;
 CREATE TABLE IF NOT EXISTS `uzivatele` (
 `id_uzivatele` int(11) NOT NULL,
   `datum_registrace` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -223,13 +159,8 @@ CREATE TABLE IF NOT EXISTS `uzivatele` (
   `heslo` varchar(1000) COLLATE utf8_czech_ci NOT NULL,
   `actived` tinyint(1) NOT NULL DEFAULT '0',
   `activ_code` varchar(1000) COLLATE utf8_czech_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
---
--- Truncate table before insert `uzivatele`
---
-
-TRUNCATE TABLE `uzivatele`;
 --
 -- Dumping data for table `uzivatele`
 --
@@ -243,36 +174,23 @@ INSERT INTO `uzivatele` (`id_uzivatele`, `datum_registrace`, `role`, `email`, `h
 --
 -- Table structure for table `zbozi_kosik`
 --
--- Creation: Feb 25, 2015 at 09:25 AM
---
 
-DROP TABLE IF EXISTS `zbozi_kosik`;
 CREATE TABLE IF NOT EXISTS `zbozi_kosik` (
 `id_zbozi_kosik` int(11) NOT NULL,
   `id_kosiku` int(11) NOT NULL,
   `id_zbozi` int(11) DEFAULT NULL,
   `mnozstvi` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
---
--- RELATIONS FOR TABLE `zbozi_kosik`:
---   `id_kosiku`
---       `kosiky` -> `id_kosiku`
---   `id_zbozi`
---       `produkty` -> `id_produktu`
---
-
---
--- Truncate table before insert `zbozi_kosik`
---
-
-TRUNCATE TABLE `zbozi_kosik`;
 --
 -- Dumping data for table `zbozi_kosik`
 --
 
 INSERT INTO `zbozi_kosik` (`id_zbozi_kosik`, `id_kosiku`, `id_zbozi`, `mnozstvi`) VALUES
-(1, 1, 1, 1);
+(1, 1, 1, 1),
+(2, 1, 2, 3),
+(10, 2, 3, 3),
+(11, 2, 2, 1);
 
 --
 -- Indexes for dumped tables
@@ -339,12 +257,12 @@ MODIFY `id_aktuality` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 -- AUTO_INCREMENT for table `clanky`
 --
 ALTER TABLE `clanky`
-MODIFY `id_clanku` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `id_clanku` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `kosiky`
 --
 ALTER TABLE `kosiky`
-MODIFY `id_kosiku` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id_kosiku` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `materialy`
 --
@@ -354,7 +272,7 @@ MODIFY `id_materialu` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT for table `obrazky`
 --
 ALTER TABLE `obrazky`
-MODIFY `id_obrazku` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `id_obrazku` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `produkty`
 --
@@ -364,12 +282,12 @@ MODIFY `id_produktu` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- AUTO_INCREMENT for table `uzivatele`
 --
 ALTER TABLE `uzivatele`
-MODIFY `id_uzivatele` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
+MODIFY `id_uzivatele` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `zbozi_kosik`
 --
 ALTER TABLE `zbozi_kosik`
-MODIFY `id_zbozi_kosik` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id_zbozi_kosik` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 --
 -- Constraints for dumped tables
 --
