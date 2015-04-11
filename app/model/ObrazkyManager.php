603fb6efbb472c87ae177d9588cac65876c2f3a6
KrystofStekovic <krystofstekovic@gmail.com>
2015-04-11 11:07:13 +0200

    neco

---
+++ app/model/ObrazkyManager.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Model;
+
+use Nette;
+
+/**
+ * Description of ObrazkyManager
+ *
+ * @author sasa
+ */
+class ObrazkyManager extends Nette\Object {
+    //put your code here
+    
+    public function addObrazek(){
+        
+    }
+}
